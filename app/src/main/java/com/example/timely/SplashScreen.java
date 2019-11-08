f0197d385fafc84bf7c27f10d18f2a536b509833
Zach Mabry <zmabry2@illinois.edu>
2019-11-08 16:52:44 -0600

    splash commit

---
+++ app/src/main/java/com/example/timely/SplashScreen.java
@@ -0,0 +1,27 @@
+package com.example.timely;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        // Make Database/Network Calls and Transfer to Intent
+
+        // For now, just a timer and send to next activity
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException ie) {
+            ie.printStackTrace();
+        }
+
+        Intent mainScreen = new Intent();
+
+    }
+}
