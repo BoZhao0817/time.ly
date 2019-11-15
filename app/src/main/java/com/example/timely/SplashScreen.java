8d154f45b4b0c87d419b469874b7099de264ddd9
zmabry2 <zmabry2@illinois.edu>
2019-11-15 15:41:16 -0600

    splash screen fix

--- app/src/main/java/com/example/timely/SplashScreen.java
+++ app/src/main/java/com/example/timely/SplashScreen.java
@@ -14,7 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_screen);
 
