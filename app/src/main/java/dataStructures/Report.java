6eef3f099caac385dcacbfe8850dcdb3cc7eb39f
Zach Mabry <zmabry2@illinois.edu>
2019-11-18 05:18:13 -0600

    added date to Report

--- app/src/main/java/dataStructures/Report.java
+++ app/src/main/java/dataStructures/Report.java
@@ -1,10 +1,12 @@
 package dataStructures;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.UUID;
 
 public class Report {
     public UUID id;
+    public Date date;
     public int total_estimate;
     public int total_actual;
     public ArrayList<Integer> estimates;
@@ -12,6 +14,7 @@ public class Report {
 
     public Report(Presentation p) {
         id = UUID.randomUUID();
+        date = new Date();
         total_actual = p.duration;
         for (Section s : p.sections) {
             actuals.add(s.duration);
